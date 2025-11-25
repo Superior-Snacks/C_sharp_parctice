dd091ef067baa956b46f16a8ae5559d2a71312e1
Superior-Snacks <joningiwork@gmail.com>
2025-11-25 18:11:25 +0000

    s

--- Operators/Program.cs
+++ Operators/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+Operators Arithmatic, Comparison, assignment, logical, bitwise
+
+Arithmatic:
+add +, 1+2
+subtract -, 1-2
+multiply *, 1*2
+divide /, 1/2
+Remainder %, 1%2
+increment ++, a = a+1 int a = 1; int b = a++; or int a = 1; int b = ++a;
+decrement --, a = a-1
+
+Comparison:
+equal ==, a==b
+notequal !=, a!=b
+greater >, a>b
+greater or equal >=, a>=b
+lesser <, a<b 
+lesser or equal <= a<=b
+
+assignment:
+assignment =, a = 1
+addition assignment +=, a += 3
+subtraction assignment -=,
+multiplication assignment *=
+division assignment /=
+
+logical:
+and &&, a&&b
+or ||, a||b
+not !, !a
+
+bitwise:
+and &, a& b
+or |, a | b
+*/
