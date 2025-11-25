1dc373dfe8d5c78304d8289d5fea35ec468b6314
Superior-Snacks <joningiwork@gmail.com>
2025-11-25 17:46:14 +0000

    s

--- typeConversion/Program.cs
+++ typeConversion/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace TypeConvertions{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte b = 1;
+            int i = b;
+            System.Console.WriteLine(i);
+        }
+    }
+}
