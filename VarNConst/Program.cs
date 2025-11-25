009b778f7511b159174456462f33067cc7a92f0e
Superior-Snacks <joningiwork@gmail.com>
2025-11-25 15:51:56 +0000

    s

--- VarNConst/Program.cs
+++ VarNConst/Program.cs
@@ -1,3 +1,22 @@
-﻿int number;
-int Number = 10;
+﻿
+// vars no consts
+int numberSmall; // camel case convention
+const int NumberBig = 10; // pascal case convention
 const float Pi = 3.14;
+
+//MIKILVÆGT FYRIR REAL numberSmall
+float aNumber = 1.4f; // þarf að bæta f fyrir aftan því c# compiler telur öll real num sem double
+decimal aBigNumber = 10.9m; // sama hér m fyrir aftan til að passa að talan er decimal
+double normalNumber = 6; // þarf ekkert auka double er default
+
+byte num = 255; // max num
+
+num = num + 1; // 0 over flowr
+
+checked
+{
+    byte safeByte = 255;
+    safeByte = safeByte + 1; //exception þarft ekk ef það er möguleiki þá nota ér small data type
+}
+
+// identifiers 
