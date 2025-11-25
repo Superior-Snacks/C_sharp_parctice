bcb8a7de0d069d26ae39e537989e0e0f1a26d448
Superior-Snacks <joningiwork@gmail.com>
2025-11-25 14:58:27 +0000

    s

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("You need to get the hang of this!");
+Console.WriteLine("BROTHER");
--- HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+417f3e1bba312bce1e0cf181e02a6312a313bd16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4974e3416649af1b5d3a053f11597073431dc780")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
