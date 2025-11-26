9d15bff7d101393d550fdfefb300faad1355fa96
Superior-Snacks <joningiwork@gmail.com>
2025-11-26 16:20:58 +0000

    s

--- ConsturctorPrac/Program.cs
+++ ConsturctorPrac/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//wtf eru constructors
+// meathod se er called um leið og class er created þ.e þegar class er fyrst notað fyrir object init 
+//why? init það þarf að fylla sum fields
+namespace ConstructiorPractice
+{
+    public class Customer
+    {
+        public int id;
+        public string Name;
+        public Customer(string name) //constructorinn hefur nákvæmlega sama nafn of classinn einginn return type
+        {                   // staður til að init defaoult stillir allt í 0, False, None
+            this.Name = name;
+        }
+        public Customer(int id, string name)
+        {
+            
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer("thomas");
+        }
+    }
+}
