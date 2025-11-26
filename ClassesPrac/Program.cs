95be8ef818bb000fdf42f090f80f2dd11786ba94
Superior-Snacks <joningiwork@gmail.com>
2025-11-26 15:29:35 +0000

    s

---
+++ ClassesPrac/Program.cs
@@ -0,0 +1,10 @@
+﻿//held að classes eru bara functinos í python never mind það er meira eins og classes í python 
+//ja fo sho u dummy
+
+namespace classesPractice
+{
+    class Program
+    {
+        
+    }
+}
