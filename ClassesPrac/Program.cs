4fbd3f161c174fdf2a9978dc3ce470ce87f888e4
Superior-Snacks <joningiwork@gmail.com>
2025-11-26 15:31:12 +0000

    s

--- ClassesPrac/Program.cs
+++ ClassesPrac/Program.cs
@@ -1,6 +1,10 @@
 ﻿//held að classes eru bara functinos í python never mind það er meira eins og classes í python 
 //ja fo sho u dummy
 
+// type sem maður definar, havð er í því
+//data represented by fields?
+// behaviour sem er functionar sem ég minntist á áður
+
 namespace classesPractice
 {
     class Program
