ca38607470628accae0424abbc4de32ecc7c6009
Superior-Snacks <joningiwork@gmail.com>
2025-11-27 18:50:34 +0000

    s

--- scope/Program.cs
+++ scope/Program.cs
@@ -9,6 +9,7 @@ namespace Variables
             const float pi = 3.14f;
             const float e = 2.71f;
             System.Console.WriteLine(pi);
+            Console.WriteLine(e);
         }
     }
 }
