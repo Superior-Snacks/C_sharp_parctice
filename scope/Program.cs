ff287a35165a7f0486f2c9a407827e1a8db74bf6
Superior-Snacks <joningiwork@gmail.com>
2025-11-27 18:50:04 +0000

    Add standard Visual Studio gitignore

--- scope/Program.cs
+++ scope/Program.cs
@@ -7,6 +7,7 @@ namespace Variables
         static void Main(string[] args)
         {
             const float pi = 3.14f;
+            const float e = 2.71f;
             System.Console.WriteLine(pi);
         }
     }
