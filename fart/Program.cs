b9b7e8964bbaebcbcb505f23856c67c67cb0af9e
Superior-Snacks <joningiwork@gmail.com>
2025-11-28 18:54:38 +0000

    s

--- fart/Program.cs
+++ fart/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace WorldHi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 56;
+            string name = "fartboyz";
+            Console.WriteLine("pissoff");
+            Console.WriteLine(34);
+            Console.WriteLine("fartboy");
+            Console.WriteLine("aah yes it is the {0} {1}", number, name);
+        }
+    }
+}
