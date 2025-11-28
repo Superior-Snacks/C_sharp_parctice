019c4551bf50ef4ee5609674a96cb4664663e775
Superior-Snacks <joningiwork@gmail.com>
2025-11-28 19:20:12 +0000

    s

--- fart/Program.cs
+++ fart/Program.cs
@@ -5,12 +5,32 @@ namespace WorldHi
     {
         static void Main(string[] args)
         {
-            byte number = 56;
-            string name = "fartboyz";
-            Console.WriteLine("pissoff");
-            Console.WriteLine(34);
-            Console.WriteLine("fartboy");
-            Console.WriteLine("aah yes it is the {0} {1} you are legendary", number, name);
+            int age;
+            age = 23;
+            int betterage = 23;
+
+            long bigNum = -90000000000L;
+            Console.WriteLine("{0}, {1}", age, betterage);
+            Console.WriteLine(bigNum);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(long.MaxValue);
+
+            double neg = 55.23;
+            Console.WriteLine(neg);
+            Console.WriteLine(double.MaxValue);
+
+            float evenLarger = 5.9999999F;
+
+            Console.WriteLine(evenLarger);
+            Console.WriteLine(float.MaxValue);
+
+            decimal curr = 14.99M;
+            Console.WriteLine(curr);
+            Console.WriteLine(decimal.MaxValue);
+
+
+
+            Console.ReadLine();
         }
     }
 }
