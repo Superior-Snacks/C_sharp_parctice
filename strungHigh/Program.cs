04ce1b96636a85d1bc21dd6040f52cd8552e29ff
Superior-Snacks <joningiwork@gmail.com>
2025-11-28 19:37:14 +0000

    s

--- strungHigh/Program.cs
+++ strungHigh/Program.cs
@@ -5,10 +5,19 @@
         static void Main(string[] args)
         {
             string name = "thomas";
-            char letter = "q";
+            char letter = 'c';
 
+            Console.WriteLine("your name is {0} the letter for you is {1}", name, letter);
             Console.WriteLine(name);
             Console.WriteLine(letter);
+
+            string textAge = "-48";
+            int age = Convert.ToInt32(textAge);
+            age += 100;
+            age--;
+            --age;
+            Console.WriteLine(age);
+            Console.WriteLine(textAge);
 
             Console.ReadLine();
         }
