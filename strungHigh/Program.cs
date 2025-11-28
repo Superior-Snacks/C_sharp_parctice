d6b0e35635c6cf0c6697bbc664ba3da22c8ff7de
Superior-Snacks <joningiwork@gmail.com>
2025-11-28 19:29:52 +0000

    s

---
+++ strungHigh/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace StringChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "thomas";
+            char letter = "q";
+
+            Console.WriteLine(name);
+            Console.WriteLine(letter);
+
+            Console.ReadLine();
+        }
+    }
+}
