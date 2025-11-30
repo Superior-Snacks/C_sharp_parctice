88f73c168016b061c035e88c0e3b18b8a666a8a7
Superior-Snacks <joningiwork@gmail.com>
2025-11-30 19:40:17 +0000

    s

---
+++ EXERCISEStoringUserData/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace storingUserData
+{
+    class Program
+    {
+        /*
+        Define Variable to hold name
+        Define variable to hold Phone number
+        Define variable to hold age
+        print variables line by line to the screen
+        Extra define variables using the var keyword
+         */
+        static void Main(string[] args)
+        {
+            //fist way
+            string name = "Theodore Rosavelt";
+            string phoneNumber= "0123456789";
+            int age = 60;
+
+            Console.WriteLine(name);
+            Console.WriteLine(phoneNumber);
+            Console.WriteLine(age);
+            Console.WriteLine("press enter to see it again");
+
+            Console.ReadLine();
+            
+            var Vname = "Thomas Jefferson";
+            var VPhoneNumber = "0123456789";
+            var Vage = 48; // nú þegar int32
+            
+            Console.WriteLine(Vname);
+            Console.WriteLine(VPhoneNumber);
+            Console.WriteLine(Vage);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ constKeywords/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace constKeywords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ef talan er notuð oft og þarf alltaf að vera eins
+            //í compiler const er bara find and replace tæknilega séð
+            const int vat = 20;
+            const double percentVAT = vat / 100D;
+
+            int balance = 1000;
+
+            Console.WriteLine(balance * (vat / 100D) ); //ef þarf að fá kommu stafi muna hverskonar tala etta er
+            Console.WriteLine(balance * percentVAT);
+            //vat = 10;
+            Console.WriteLine(vat);
+            Console.ReadLine();
+        }
+    }
+}
