0291444ef208e5d8d1714aa44a76a61b3c887ef7
Superior-Snacks <joningiwork@gmail.com>
2025-11-30 18:58:53 +0000

    s

--- Operations/Program.cs
+++ Operations/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
+            double age = 24;
+            age++;
+            Console.WriteLine(age);
+            age = age + 1;
+            Console.WriteLine(age);
+            age += 10;
+            Console.WriteLine(age);
+            age--;
+            Console.WriteLine(age);
+            age *= 11;
+            Console.WriteLine(age);
+            age /= 10;
+            Console.WriteLine(age);
 
+            string name = "abba";
+            name += " is a good band";
+            Console.WriteLine(name);
+
+            char letter = 'a';
+            letter += 'b';
+            Console.WriteLine(letter);
+
+            int i = 0;
+            i++;
+            Console.WriteLine(i);
+
+            Console.WriteLine(i++); //returnar i svo ++ þannig enging breyting sést
+            Console.WriteLine(++i); // fix?
             Console.ReadLine();
         } 
     }
