3c8ecc47223e49a5080fc52171f0f0c67e57e0b5
Superior-Snacks <joningiwork@gmail.com>
2025-11-30 19:54:34 +0000

    s

---
+++ InputsOutputs/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace InputsOutputs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //common knowlage the remainder of a number divided by 2 is o if even and 1 if odd
+            int value = 11;
+
+            int check = value % 2;
+
+            Console.WriteLine(check);
+
+            if (check == 1)
+            {
+                Console.WriteLine("Odd");
+            }
+            else if (check == 0)
+            {
+                Console.WriteLine("Even");
+            }
+
+            Console.WriteLine("___________________________________");
+            value += 1;
+            check = value % 2;
+            Console.WriteLine(check);
+            if (check == 1)
+            {
+                Console.WriteLine("Odd");
+            }
+            else if (check == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            Console.ReadLine();
+        }
+    }
+}
