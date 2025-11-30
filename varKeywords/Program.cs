fb5b248b2fe17529b5f352b74db689539c352ea1
Superior-Snacks <joningiwork@gmail.com>
2025-11-30 19:10:28 +0000

    s

---
+++ varKeywords/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace varKeywords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int age = 24;
+            //int age; leagal
+            var age = 24; // þarf value
+            Console.WriteLine(age);
+
+            //long bigNumber = 900000000000L;
+            var bigNumber = 9000000L;
+            Console.WriteLine(bigNumber);
+
+            var negative = -55.8D; // double
+            Console.WriteLine(negative);
+
+            var precision = 5.9999999900F; // float
+            Console.WriteLine(precision);
+
+            var money = 14.99M; //important
+            Console.WriteLine(money);
+
+            var name = "thomas";
+            Console.WriteLine(name);
+
+            var letter = 'c';
+            Console.WriteLine(letter);
+            Console.ReadLine();
+        }
+    }
+}
