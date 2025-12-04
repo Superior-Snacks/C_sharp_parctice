196d02f4a238c55702797a5b4c26a251c3efda88
Superior-Snacks <joningiwork@gmail.com>
2025-12-04 19:03:50 +0000

    s

--- InputsOutputs/Program.cs
+++ InputsOutputs/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("yo my name it updawg");
+            Console.WriteLine("yo my name is updawg");
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("good evening " + name);
+
+            Console.ReadLine();
         }
     }
 }
