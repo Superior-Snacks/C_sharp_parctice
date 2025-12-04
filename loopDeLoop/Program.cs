d085d0258af85bb85caa2e0b97b3f2550bd1de72
Superior-Snacks <joningiwork@gmail.com>
2025-12-04 19:55:57 +0000

    s

--- loopDeLoop/Program.cs
+++ loopDeLoop/Program.cs
@@ -7,6 +7,12 @@
             for (int i = 0; i < 5; i++)
             {
                 Console.WriteLine("yo!");
+                Console.WriteLine(i);
+            }
+
+            for (int i = 0; i <= 10; i += 2)
+            {
+                Console.WriteLine(i);
             }
 
             Console.ReadLine();
