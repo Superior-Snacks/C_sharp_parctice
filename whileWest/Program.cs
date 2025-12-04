0e8122b1ec8e3bdad0865bc8b303f25aab9a958f
Superior-Snacks <joningiwork@gmail.com>
2025-12-04 20:06:47 +0000

    s

---
+++ whileWest/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace whileWest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 0;
+            while ( input != 666)
+            {
+                Console.Write("what is the number of the devil? ");
+                input = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("YOU FOUND THE NUMBER");
+            Console.ReadLine();
+        }
+    }
+}
