61e13c9dc4c90c88e73628736325fb9f7eeb398e
Superior-Snacks <joningiwork@gmail.com>
2025-12-04 19:51:28 +0000

    s

---
+++ weekDays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Transactions;
+
+namespace WeekDays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter day of the week: ");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            switch (day)
+            {
+                case 1: Console.WriteLine("mon");
+                    break;
+                case 2: Console.WriteLine("tue");
+                    break;
+                case 3: Console.WriteLine("wed");
+                    break;
+                case 4: Console.WriteLine("thu");
+                    break;
+                case 5: Console.WriteLine("fri");
+                    break;
+                case 6: Console.WriteLine("sat");
+                    break;
+                case 7: Console.WriteLine("sun");
+                    break;
+                default: Console.WriteLine("ERROR INVALID INPUT");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
