18fbafa114810fc4c57b5b55143ad6f684d48c9d
Superior-Snacks <joningiwork@gmail.com>
2025-12-04 19:54:17 +0000

    s

--- loopDeLoop/Program.cs
+++ loopDeLoop/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace loopDeLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("yo!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
