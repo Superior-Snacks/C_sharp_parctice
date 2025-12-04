8a1037fe15042034a314747a3c57b066fc723c3b
Superior-Snacks <joningiwork@gmail.com>
2025-12-04 19:08:48 +0000

    s

--- InputsOutputs/Program.cs
+++ InputsOutputs/Program.cs
@@ -4,11 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("yo my name is updawg");
+            //Console.WriteLine("yo my name is updawg");
 
             Console.Write("Enter your name: ");
             string name = Console.ReadLine();
-            Console.WriteLine("good evening " + name);
+            //Console.WriteLine("good evening " + name);
+
+            Console.Write("and what is your age: ");
+            string age = Console.ReadLine();
+            //Console.WriteLine("an outrageous age of " + age);
+
+            //sleppa line svo allt endar á sömu línu
+            Console.Write("your name is ");
+            Console.Write(name);
+            Console.Write(" and your age is "); 
+            Console.Write(age);
 
             Console.ReadLine();
         }
