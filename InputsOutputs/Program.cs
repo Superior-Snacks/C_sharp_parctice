66cf5768765c58a9f55407bcd2af07462d2ac9db
Superior-Snacks <joningiwork@gmail.com>
2025-12-04 19:29:15 +0000

    s

--- InputsOutputs/Program.cs
+++ InputsOutputs/Program.cs
@@ -49,6 +49,28 @@
                 }
             }
 
+            Console.WriteLine("MATH TIME!!!");
+            Console.WriteLine("enter two numbers");
+            Console.Write("number 1: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("number 2: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            int correct = number1 + number2;
+
+            Console.WriteLine();
+            Console.Write("ok now calculate " + number1 + " + " + number2 " = ");
+            int anwser = Convert.ToInt32(Console.ReadLine());
+
+            if (anwser == correct)
+            {
+                Console.WriteLine("!!!!! CORRECT YOU DONE DID IT !!!!!!!");
+            }
+            else
+            {
+                Console.WriteLine("boo ass at this man");
+            }
+
                 Console.ReadLine();
         }
     }
