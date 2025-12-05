6974ccd9f4600dbcc7297c01e1279b4ddaedbf59
Superior-Snacks <joningiwork@gmail.com>
2025-12-05 19:41:44 +0000

    s

--- conditionalOperators/Program.cs
+++ conditionalOperators/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
+            Console.Write("type age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
 
+            //the conditional operator, string nameVar = condition ? "trueOutput" : "falseOutput";
+            //aðalega til að einfalda if then statements mikilvægt fyrir lesanlegan kóða
+            string output = age > 0 && age < 150 ? "Valid Age!" : "Suspect age?";
+            Console.WriteLine(output);
             Console.ReadLine();
         }
     }
