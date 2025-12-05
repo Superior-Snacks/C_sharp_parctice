76ea1f56c81624562b385de947c566b3ab148cf5
Superior-Snacks <joningiwork@gmail.com>
2025-12-05 19:55:16 +0000

    s

---
+++ formatting/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace formatting
+{
+    class Project
+    {
+        static void Main(string[] args)
+        {
+            double number = 15.123456789;
+
+            Console.WriteLine(number);
+            Console.WriteLine(string.Format("{0:0.0}",number));
+            Console.WriteLine(string.Format("{0:0.00}", number));
+            Console.WriteLine(string.Format("{0:0.000} {1:0.00000}", number, number));
+
+            Console.ReadLine();
+        }
+    }
+}
