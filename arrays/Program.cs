167296176bffdadf8c939edb0c304a690e9e5777
Superior-Snacks <joningiwork@gmail.com>
2025-12-09 19:48:37 +0000

    s

--- arrays/Program.cs
+++ arrays/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
