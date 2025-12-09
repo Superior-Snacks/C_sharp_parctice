ef2305b6d1ca4831ef34bf796656205753077795
Superior-Snacks <joningiwork@gmail.com>
2025-12-09 15:20:05 +0000

    s

--- tryAndParce/Program.cs
+++ tryAndParce/Program.cs
@@ -8,25 +8,22 @@ namespace tryAndParce
         {
             Console.Write("write number:");
             string getNumber = Console.ReadLine();
-            int number = 0;
+            
+            //int number = 0;
+            //bool sucsess = int.TryParse(getNumber, out number);
 
-            bool sucsess = int.TryParse(getNumber, out number);
-
-            if (sucsess)
+            if (int.TryParse(getNumber, out int number))
             {
-                Console.WriteLine(sucsess);
+                Console.WriteLine("True");
                 Console.WriteLine("sucessful input!!!");
                 Console.WriteLine(number);
             }
             else
             {
-                Console.WriteLine(sucsess);
+                Console.WriteLine("False");
                 Console.WriteLine("YOU FAIL TO LIVE!!!!1");
             }
-                Console.WriteLine("");
 
-            Console.WriteLine(sucsess);
-            Console.WriteLine(number);
             Console.ReadLine();
         }
     }
