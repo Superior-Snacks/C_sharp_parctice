8b2a36ce382c9428679dd3892dc6f86916e5e539
Superior-Snacks <joningiwork@gmail.com>
2025-12-09 17:24:46 +0000

    s

--- stringFormatting/Program.cs
+++ stringFormatting/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace stringFormatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadLine();
+        }
+    }
+}
