16d2ebf86ea5f7005f0236ba57848e0df317be09
Superior-Snacks <joningiwork@gmail.com>
2025-12-09 17:30:14 +0000

    s

--- stringFormatting/Program.cs
+++ stringFormatting/Program.cs
@@ -4,6 +4,16 @@
     {
         static void Main(string[] args)
         {
+            string name = "charles";
+            int age = 43;
+
+            Console.WriteLine("your name is {0}, and your age is a surpirsingly normal {1}", name, age); //hann vill
+            Console.WriteLine($"your name is {name}, and your age is a surpirsingly normal {age}"); // betra?
+
+            Console.WriteLine("name: {0}\nage {1}", name, age);
+
+            Console.WriteLine($"name: {name}\nage {age}");
+
 
             Console.ReadLine();
         }
