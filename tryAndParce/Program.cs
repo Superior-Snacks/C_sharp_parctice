7922c5e4206536a7a3eac14b6573c4c2aed1b5df
Superior-Snacks <joningiwork@gmail.com>
2025-12-09 15:16:11 +0000

    s

--- tryAndParce/Program.cs
+++ tryAndParce/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace tryAndParce
+﻿using System.Threading.Channels;
+
+namespace tryAndParce
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("write number:");
+            string getNumber = Console.ReadLine();
+            int number = 0;
+
+            bool sucsess = int.TryParse(getNumber, out number);
+            Console.WriteLine("");
+
+            Console.WriteLine(sucsess);
+            Console.WriteLine(number);
             Console.ReadLine();
         }
     }
