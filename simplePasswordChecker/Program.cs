756f00c5cf398153979d464da154f85a86edde12
Superior-Snacks <joningiwork@gmail.com>
2025-12-09 19:35:55 +0000

    s

--- simplePasswordChecker/Program.cs
+++ simplePasswordChecker/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace simplePasswordChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Password: ");
+            string password1 = Console.ReadLine();
+
+            string password2;
+            do
+            {
+                Console.Write("Re-Enter Password: ");
+                password2 = Console.ReadLine();
+            }
+            while (!string.Equals(password1, password2));
+
+            Console.WriteLine();
+            Console.WriteLine($"CONGRATS Password set as {password1}");
+            
+            Console.ReadLine();
+        }
+    }
+}
