f4d6acfd95e21e221b3009540b1f1849706765cf
Superior-Snacks <joningiwork@gmail.com>
2025-12-10 20:04:30 +0000

    s

--- arrays/Program.cs
+++ arrays/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("yo");
+            int num1 = 1;
+            int num2 = 2;
+            int num3 = 3;
+
+            int[] numbers = new int[3];
+
+            Console.Write("Enter a number: ");
+            numbers[0] = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a number: ");
+            numbers[1] = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a number: ");
+            numbers[2] = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{numbers[0]}, {numbers[1]}, {numbers[2]}");
+
+
             Console.ReadLine();
         }
     }
