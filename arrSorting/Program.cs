8329d4a00fbd5211ff4e7f9221f77ebaf1de929d
Superior-Snacks <joningiwork@gmail.com>
2025-12-17 17:16:27 +0000

    s

--- arrSorting/Program.cs
+++ arrSorting/Program.cs
@@ -4,6 +4,15 @@
     {
         static void Main(string[] args)
         {
+            int[] numbers = new int[10]
+            {
+                1,2,3,4,5,6,7,8,9,10
+            };
+
+            foreach (int item in numbers)
+            {
+                Console.WriteLine(item);
+            }
             Console.ReadLine();
         }
     }
