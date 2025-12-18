e3b30a99a9dea0dfbcd2f7159541a0fbc4e06da3
Superior-Snacks <joningiwork@gmail.com>
2025-12-18 18:35:04 +0000

    s

---
+++ doubleDictionaries/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace doubleDictionaries
+{
+    class Program
+    {
+        static void Main(string args)
+        {
+            Dictionary<int, string> names = new Dictionary<int, string>();
+            names.Add(0, "Jón");
+            names.Add(1, "Jón1");
+            names.Add(2, "Jón2");
+            names.Add(3, "Jón3");
+            KeyValuePair<int, string> got = names.ElementAt(2);
+            Console.WriteLine($"{got.Key} - {got.Value}");
+            Console.ReadLine();
+        }
+    }
+}
