5d789cde6db12bf7159e4c3944e0303150aac3b9
Superior-Snacks <joningiwork@gmail.com>
2025-12-18 20:28:35 +0000

    s

--- functionParamiters/Program.cs
+++ functionParamiters/Program.cs
@@ -4,6 +4,28 @@
     {
         static void Main(string[] args)
         {
+            string thePlan = "Two study sessions 1.5 hours long";
+            string timeing = "vera mættur kl 8:30";
+            string teikna = "Ég vil teikna á morgun";
+            string method = "Teikna með 30 min timer";
+
+            List<string> = new List<string>()
+            {
+                thePlan,
+                timeing,
+                teikna,
+                method
+            }
+
+            static void writeToConsole(string sent)
+            {
+                for (int i = 0; i < sent.Length; i++)
+                {
+                    Console.Write(sent[i]);
+                }
+            }
+
+            
             Console.ReadLine();
         }
     }
