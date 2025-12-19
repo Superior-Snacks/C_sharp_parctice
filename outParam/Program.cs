a32005bd20f726bd02fdd5b0fa460b535ab1d717
Superior-Snacks <joningiwork@gmail.com>
2025-12-19 14:52:31 +0000

    s

--- outParam/Program.cs
+++ outParam/Program.cs
@@ -4,6 +4,14 @@
     {
         static void Main(string[] args)
         {
+            int num = 5;
+            test(num);
+            Console.WriteLine(num);
+            Console.WriteLine(test(num));
+            static int test(int num)
+            {
+                num = 0;
+            }
             Console.ReadLine();
         }
     }
