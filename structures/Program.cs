127d54b2207643350ca01479e3cfd262e3913d4a
Superior-Snacks <joningiwork@gmail.com>
2025-12-20 16:21:36 +0000

    s

--- structures/Program.cs
+++ structures/Program.cs
@@ -2,9 +2,24 @@
 {
     class Program
     {
+        struct Person
+        {
+            public string name;
+            public int age;
+            public int birthday;
+
+            public Person(string name, int age, int birthday)
+            {
+                this.name = name;
+                this.age = age;
+                this.birthday = birthday;
+            }
+        }
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("yo");
             Console.ReadLine();
         }
     }
