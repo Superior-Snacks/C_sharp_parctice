7f692dfbfaadaa5db13d98ae6578e6f44b0ab7a7
Superior-Snacks <joningiwork@gmail.com>
2025-12-22 20:55:46 +0000

    beginner course finished

--- structures/Program.cs
+++ structures/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace structures
+﻿using System.Threading.Channels;
+
+namespace structures
 {
     class Program
     {
@@ -8,12 +10,25 @@
             private string name;
             private int age;
 
+            public string Name
+            {
+                get
+                {
+                    return name;
+                }
+                set
+                {
+                    name = value;
+                }
+
+            }
+            public int Age { get => age; set => age = value; }
 
 
             public Person()
             {
-                this.name = default;
-                this.age = default;
+                Name = default;
+                Age = default;
             }
             public Person(string name)
             {
@@ -30,20 +45,26 @@
                 this.name = name;
                 this.age = age;
             }
-                        public void setName(string name)
-            {
-                if (!string.IsNullOrEmpty(name))
-                {
-                    this.name = name;
-                }
-                else
-                {
-                    this.name = "invalid name";
-                }
-            }
             public string ReturnDetails()
             {
                 return $"name: {name}, age: {age}";
+            }
+            public override string ToString()
+            {
+                return $"name: {name}, age: {age}";
+            }
+            public override bool Equals(object? obj)
+            {
+                if (obj is Person)
+                {
+                    Person person = obj as Person;
+                    return Name.Equals(person.Name) && Age == person.Age;
+                }
+                else
+                {
+                    return false;
+                }
+                
             }
         }
 
@@ -51,10 +72,28 @@
         static void Main(string[] args)
         {
             Person person = new Person("abba", 23);
-
-            person.setName("tommy");
+            person.Name = "Frank";
+            Console.WriteLine(person.Name);
+            person.Age = 400;
+            Console.WriteLine(person.Age);
             Console.WriteLine(person.ReturnDetails());
-            Console.ReadLine();
+            Console.WriteLine(person);
+
+            Person test = new Person("Frank", 40);
+
+            if (person.Equals(test))
+            {
+                Console.WriteLine(person);
+                Console.WriteLine("SAME");
+            }
+            else
+            {
+                Console.WriteLine("NO BUENO");
+            }
+
+
+
+                Console.ReadLine();
         }
     }
 }
