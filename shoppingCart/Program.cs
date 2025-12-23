cf4080426cfd6bcac456d416d439c58be4a7c178
Superior-Snacks <joningiwork@gmail.com>
2025-12-23 17:17:44 +0000

    s

--- shoppingCart/Program.cs
+++ shoppingCart/Program.cs
@@ -5,15 +5,22 @@
         static void Main(string[] args)
         {
             string title = "TODO";
-            string t1 = "shoppingcart:";
+            string t1 = "shoppingcart: ";
             string t1Content = "adding list of items";
-            string t2 = "market:";
+            string t2 = "market: ";
             string t2Content = "a dictionary of items for sale with price and quantiy availabel";
-            string t3 = "logic:";
+            string t3 = "logic: ";
             string t3Content = "add only to cart if spelled correctly, " +
                 "remove from stock when added to cart " +
                 "and do not allow to take more than in stock" +
                 "have a checkout process";
+            Console.WriteLine(title);
+            Console.Write(t1);
+            print(t1Content);
+            Console.WriteLine(t2);
+            print(t2Content);
+            Console.WriteLine(t3);
+            print(t3Content);
             Console.ReadLine();
         }
         static void print(string sentance)
@@ -23,6 +30,7 @@
                 Thread.Sleep(200);
                 Console.Write(sentance[i]);
             }
+            Console.WriteLine();
         }
     }
 }
