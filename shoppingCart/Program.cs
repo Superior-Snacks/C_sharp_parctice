7b41785518fdd93dc4d561d5cf8af6805e0604f2
Superior-Snacks <joningiwork@gmail.com>
2025-12-23 17:09:12 +0000

    s

---
+++ shoppingCart/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace shoppingCart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string title = "TODO";
+            string t1 = "shoppingcart:";
+            string t1Content = "adding list of items";
+            string t2 = "market:";
+            string t2Content = "a dictionary of items for sale with price and quantiy availabel";
+            string t3 = "logic:";
+            string t3Content = "add only to cart if spelled correctly, "
+            Console.ReadLine();
+        }
+    }
+}
