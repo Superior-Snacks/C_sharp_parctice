45c431a77516dcc9c35e6025dae90dae1d104d03
Superior-Snacks <joningiwork@gmail.com>
2026-01-19 12:26:58 +0000

    s

--- FieldValidatorAPI/Class1.cs
+++ FieldValidatorAPI/Class1.cs
@@ -1,4 +1,6 @@
-﻿namespace FieldValidatorAPI
+﻿using static FieldValidatorAPI.CommonFieldValidatorFunctions;
+
+namespace FieldValidatorAPI
 {
     public class CommonFieldValidatorFunctions
     {
@@ -7,5 +9,16 @@
         public delegate bool DateValidDel(string fieldVal, out DateTime ValidDateTime);
         public delegate bool PatternMatchDel(string fieldVal, string pattern);
         public delegate bool CompareFieldsValidDel(string fieldVal, string fieldValCompare);
+    }
+
+    public class CommmonFieldValidatorFunctinos
+    {
+        private static RequierdValidDel _requierdValidDel = null;
+        private static StringLengthValidDel _stringLengthValidDel = null;
+        private static DateValidDel _dateValidDel = null;
+        private static PatternMatchDel _patternMatchDel = null;
+        private static CompareFieldsValidDel _compareFieldsValidDel = null;
+
+
     }
 }
