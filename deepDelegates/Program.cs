041ec40a3f6e84bbce4f94c1bec0e4bd11c96c2a
Superior-Snacks <joningiwork@gmail.com>
2026-01-19 11:44:44 +0000

    s

---
+++ deepDelegates/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace deepDelegates
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("yo");
+            Console.ReadLine();
+        }
+    }
+}
