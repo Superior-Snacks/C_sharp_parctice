aa2ea6c1d7df3b0a37a88d0cf310b9a48627e323
Superior-Snacks <joningiwork@gmail.com>
2026-01-19 12:17:49 +0000

    s

---
+++ FieldValidatorAPI/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace FieldValidatorAPI
+{
+    public class CommonFieldValidatorFunctions
+    {
+        public delegate bool RequierdValidDel(string filedVal);
+        public delegate bool StringLengthValidDel(string fieldVal, int min, int max);
+        public delegate bool DateValidDel(string fieldVal, out DateTime ValidDateTime);
+        public delegate bool PatternMatchDel(string fieldVal, string pattern);
+        public delegate bool CompareFieldsValidDel(string fieldVal, string fieldValCompare);
+    }
+}
