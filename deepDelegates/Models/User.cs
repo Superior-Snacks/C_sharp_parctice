868a60e7948b754e8f13ffde7f895637db0ee94e
Superior-Snacks <joningiwork@gmail.com>
2026-01-19 12:43:00 +0000

    s

---
+++ deepDelegates/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace deepDelegates.Models
+{
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string AddressFirstline { get; set; }
+
+        public string AddressSecondline { get; set; }
+
+        public string AddressCity { get; set; }
+
+        public string PostCode { get; set; }
+
+    }
+
+}
